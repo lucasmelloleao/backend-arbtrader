c2f239838fc1ff859a083336d9c4eea9f8cbd40b
Lucas Leao <lucasmelloleao@gmail.com>
2026-09-08 16:12:57 -0300

    fix: remove remaining merge conflict markers in authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -57,12 +57,8 @@ import {
   getLatencySettings,
   updateLatencySettings,
   getLatencyTrades,
-<<<<<<< HEAD
   closeLatencyTrade,
   getLatencyLogs
-=======
-  closeLatencyTrade
->>>>>>> e43bfd250e0e875bc95d97a5c0e07a557477bcfc
 } from '../controllers/latencyArbController';
 import {
   closeStrategy as closePerpStrategy,
