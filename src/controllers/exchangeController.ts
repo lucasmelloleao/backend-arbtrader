120975d6290b9ec7565b08bdd18ae9ae715fcde9
Lucas Leao <lucasmelloleao@gmail.com>
2026-09-23 20:01:39 -0300

    lucas

--- src/controllers/exchangeController.ts
+++ src/controllers/exchangeController.ts
@@ -3,7 +3,7 @@ import ExchangeKey from '../models/ExchangeKey';
 import { AuthenticatedRequest } from '../middleware/authMiddleware';
 import { encryptSecretKey } from '../utils/encryption';
 
-const CTRADER_IDS = ['ctrader', 'pepperstone', 'fxpro', 'fxpro-ctrader', 'deriv'];
+const CTRADER_IDS = ['ctrader', 'pepperstone', 'fxpro', 'fxpro-ctrader', 'deriv', 'icmarkets', 'icmarkets-ctrader', 'ic'];
 const FIX_IDS = ['fix', 'pepperstone-fix', 'ctrader-fix'];
 const DUKASCOPY_IDS = ['dukascopy'];
 const HYPERLIQUID_IDS = ['hyperliquid'];
